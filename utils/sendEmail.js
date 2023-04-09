3c585e6c41dbffdcf3137e10dd530627baf82ab9
Vladyslav Solomakha <vladislavsolomaha08@gmail.com>
2023-04-09 18:03:38 +0300

    fix sendEmail function

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,9 +6,13 @@ const { SENDGRID_API_KEY, EMAIL_SENDER_ADDRESS } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: EMAIL_SENDER_ADDRESS };
-  await sgMail.send(email);
-  return true;
+  try {
+    const email = { ...data, from: EMAIL_SENDER_ADDRESS };
+    await sgMail.send(email);
+    return true;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = sendEmail;
